c0a4ac079414643d6ffc5eb40b9739d7ae20759f
Lauritz Tieste <lauritz6000000@gmail.com>
2024-08-29 11:42:00 +0200

    Format page

--- pages/profile/courses.vue
+++ pages/profile/courses.vue
@@ -1,56 +1,22 @@
-<!--
-❌ Responsive UI
-✅ Page Title
-✅ Translation
-❌ Animation
-✅ middleware
-
-✅ Tested on chrome
-✅ Tested on firefox
-✅ Tested on safari
-❌ Tested on android mobile
-❌ Tested on apple mobile
-
-✅ Handle loading if data already exists
-✅ Handle loading if data is empty
-✅ Display data
-✅ Handle empty state
-
-✅ Api implemented
--->
 <template>
-	<main
-		class="grid-auto gap-card container h-screen-inner min pb-container pt-container grid-rows-[auto_auto_1fr]"
-	>
-		<FormSearch
-			class="justify-self-end col-span-full"
-			placeholder="Body.SearchCourses"
-			v-model="filters.search_term"
-		/>
+  <main class="grid-auto gap-card container h-screen-inner min pb-container pt-container grid-rows-[auto_auto_1fr]">
+    <FormSearch class="justify-self-end col-span-full" placeholder="Body.SearchCourses" v-model="filters.search_term" />
 
-		<Sort
-			class="mb-card-sm col-span-full"
-			:quantity="myCourses.length"
-			:options="options"
-			@selected="onSelectedOption($event)"
-		/>
+    <Sort class="mb-card-sm col-span-full" :quantity="myCourses.length" :options="options"
+      @selected="onSelectedOption($event)" />
 
-		<template v-if="loading">
-			<CourseCardSkeleton v-for="n in 5" :key="n" />
-		</template>
+    <template v-if="loading">
+      <CourseCardSkeleton v-for="n in 5" :key="n" />
+    </template>
 
-		<template v-else-if="myCourses && myCourses.length > 0">
-			<NuxtLink
-				v-for="(course, i) of myCourses"
-				:key="i"
-				:to="`/courses/${course.id}`"
-			>
-				<CourseCard :data="course" />
-			</NuxtLink>
-		</template>
+    <template v-else-if="myCourses && myCourses.length > 0">
+      <NuxtLink v-for="(course, i) of myCourses" :key="i" :to="`/courses/${course.id}`">
+        <CourseCard :data="course" />
+      </NuxtLink>
+    </template>
 
-		<CourseCardEmptyState class="col-span-full" v-else />
-	</main>
+    <CourseCardEmptyState class="col-span-full" v-else />
+  </main>
 </template>
 
 <script lang="ts">
@@ -107,12 +73,13 @@ export default {
 
 <style scoped>
 .grid-auto {
-	display: grid;
-	grid-template-columns: minmax(0, 1fr);
+  display: grid;
+  grid-template-columns: minmax(0, 1fr);
 }
+
 @media (min-width: 425px) {
-	.grid-auto {
-		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-	}
+  .grid-auto {
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  }
 }
 </style>
