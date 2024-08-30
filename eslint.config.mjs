b9770023a3ccd8e46914854f23bac734f215c656
Lauritz Tieste <lauritz6000000@gmail.com>
2024-08-30 17:27:50 +0200

    Add eslint with formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_",
+      },
+    ],
+  },
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,46 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-  ssr: false,
-  app: {
-    head: {
-      title: "Bootstrap Academy",
-      link: [{ rel: "manifest", href: "manifest.txt" }],
-    },
-    pageTransition: { name: "page", mode: "out-in" },
-    layoutTransition: { name: "layout", mode: "out-in" },
-  },
-  css: ["~/assets/css/tailwind.css"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  runtimeConfig: {
-    public: {
-      BASE_API_URL: "https://api.test.bootstrap.academy",
-      BASE_WEB_URL: "https://test.bootstrap.academy",
-      Gleap_API_KEY: "8TVLuULNmWxZHIifA1PW6TYHUCKEb5so",
-      Vue3ReCaptcha_SITE_KEY: "6Ldb070iAAAAAKsAt_M_ilgDbnWcF-N_Pj2DBBeP",
-      NODE_ENV: "production",
-    },
-  },
-});
+	ssr: false,
+
+	app: {
+		head: {
+			title: 'Bootstrap Academy',
+			link: [{ rel: 'manifest', href: 'manifest.txt' }],
+		},
+		pageTransition: { name: 'page', mode: 'out-in' },
+		layoutTransition: { name: 'layout', mode: 'out-in' },
+	},
+
+	css: ['~/assets/css/tailwind.css'],
+
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+
+	runtimeConfig: {
+		public: {
+			BASE_API_URL: 'https://api.test.bootstrap.academy',
+			BASE_WEB_URL: 'https://test.bootstrap.academy',
+			Gleap_API_KEY: '8TVLuULNmWxZHIifA1PW6TYHUCKEb5so',
+			Vue3ReCaptcha_SITE_KEY: '6Ldb070iAAAAAKsAt_M_ilgDbnWcF-N_Pj2DBBeP',
+			NODE_ENV: 'production',
+		},
+	},
+
+	modules: ['@nuxt/eslint'],
+	eslint: {
+		config: {
+			stylistic: {
+				semi: false,
+				indent: 'tab',
+			},
+			rules: {
+				'@typescript-eslint/no-explicit-any': 'warn',
+			},
+		},
+	},
+})
